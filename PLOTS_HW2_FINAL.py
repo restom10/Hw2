9c327a93c59cfe89fa15d3daf59d8197ff18b459
Restom10 <jr.restom10@uniandes.edu.co>
2019-05-12 04:24:53 +0000

    falto version pdf

---
+++ PLOTS_HW2_FINAL.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+u1 = np.genfromtxt('U1.txt',delimiter=';')
+u2 = np.genfromtxt('U2.txt',delimiter=';')
+u3 = np.genfromtxt('U3.txt',delimiter=';')
+
+
+# In[7]:
+
+
+w1=u1[:,0]
+max1=u1[:,1]
+w2=u2[:,0]
+max2=u2[:,1]
+w3=u3[:,0]
+max3=u3[:,1]
+
+
+# In[9]:
+
+
+plt.figure()
+plt.xlim(2.5,4)
+plt.title('Edificios')
+plt.ylabel('Amplitud')
+plt.xlabel('Amplitud')
+plt.plot(w1,max1,label='Edificio 1')
+plt.plot(w2,max2,label='Edificio 2')
+plt.plot(w3,max3,label='Edificio 3')
+plt.legend()
+
+plt.savefig('Graficas de edificios.pdf')
+plt.close()
+
+
+# In[ ]:
+
+
+
+
